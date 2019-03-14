7e781e98c6a8dc317bcf8a7031f20decc2b6566e
Patrick <BigStar1994123@gmail.com>
2019-03-15 00:03:17 +0800

    53

--- Leetcode53_Maximum Subarray/Leetcode53_Maximum Subarray/Program.cs
+++ Leetcode53_Maximum Subarray/Leetcode53_Maximum Subarray/Program.cs
@@ -8,7 +8,7 @@ namespace Leetcode53_Maximum_Subarray
         {
             Console.WriteLine("Hello World!");
             var s = new Solution();
-            var result = s.MaxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 99 });
+            var result = s.MaxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 });
             Console.WriteLine(result);
 
             Console.ReadLine();

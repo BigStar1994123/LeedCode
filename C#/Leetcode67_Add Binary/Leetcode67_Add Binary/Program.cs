33c6659dd8578835b00a28ed62be8ea80af27c53
Patrick <BigStar1994123@gmail.com>
2019-03-15 00:39:58 +0800

    m

---
+++ C#/Leetcode67_Add Binary/Leetcode67_Add Binary/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Leetcode67_Add_Binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var s = new Solution();
+            var result = s.AddBinary("1010", "1011");
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+        public string AddBinary(string a, string b)
+        {
+            var j = b.Length - 1;
+            var carry = false;
+            var r = '0';
+            for (var i = a.Length-1; i >= 0; i--)
+            {
+                if (carry)
+                {
+                    var (aa, bb) = AddChar(a[i], '1');
+                }
+
+                (r, carry) = AddChar(a[i], b[j]);
+            }
+
+        }
+
+        private (char, bool) AddChar(char a, char b)
+        {
+            if (a == '1' && b == '1')
+            {
+                return ('0', true);
+            }
+            else if (a == '0' && b == '1')
+            {
+                return ('1', false);
+            }
+            else if (a == '1' && b == '0')
+            {
+                return ('1', false);
+            }
+            else
+            {
+                return ('0', false);
+            }
+        }
+    }
+}
